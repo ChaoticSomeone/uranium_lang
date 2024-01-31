d8167be70cd61069cac7faa9963a40a70fc10477
Deniz D <121238064+ChaoticSomeone@users.noreply.github.com>
2024-01-31 16:56:31 +0100

    Delete _out/main.cpp

--- _out/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include "C:/Program Files (x86)/UraniumLang//io.h" 
-int main ( ) { 
-for ( int num = 0 ; num < 10 ; num += 1 ) { 
-uranium::io :: println ( "{}" , num ) ;
-} ;
-return 0 ;
-} ;
